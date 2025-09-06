eb0c011684aeee0b294635196764ba6e41c8449f
Wilfredo Diaz <wdiazdev@gmail.com>
2025-09-06 11:40:43 -0400

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,12 @@
 import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
 import icon from "astro-icon"
-import node from "@astrojs/node"
 import react from "@astrojs/react"
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://wdiazdev.github.io",
   base: "/wdiazdev-astro",
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
+  output: "static",
+  vite: { plugins: [tailwindcss()] },
   integrations: [icon(), react()],
-
-  adapter: node({
-    mode: "standalone",
-  }),
 })
