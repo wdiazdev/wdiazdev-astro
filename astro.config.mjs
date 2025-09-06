fddeab0509f8fafb2afbda2097711865baa3a070
Wilfredo Diaz <wdiazdev@gmail.com>
2025-09-06 11:26:53 -0400

    workflow updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config"
-
 import tailwindcss from "@tailwindcss/vite"
-
 import icon from "astro-icon"
-
 import node from "@astrojs/node"
-
 import react from "@astrojs/react"
 
 // https://astro.build/config
